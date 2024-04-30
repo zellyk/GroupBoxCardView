f59e23971f50432b078ecfd22df57d0754c710dc
zellyk <zellyk@proton.me>
2024-04-30 15:17:44 -0400

    Initial Commit

---
+++ CardViewGroupBox/CardViewGroupBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewGroupBoxApp.swift
+//  CardViewGroupBox
+//
+//  Created by Jonathan Ma on 2024-04-30.
+//
+
+import SwiftUI
+
+@main
+struct CardViewGroupBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardViewGroupBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardViewGroupBox
+//
+//  Created by Jonathan Ma on 2024-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
